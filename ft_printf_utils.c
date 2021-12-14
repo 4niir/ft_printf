4af2dbc4760dafaad68bc7d5c8959e8e667abd4a
Abderrahim Boudounit <aboudoun@e1r4p5.1337.ma>
2021-12-14 20:21:13 +0100

    tkhrbi9a

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,16 +6,16 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 15:20:51 by aboudoun          #+#    #+#             */
-/*   Updated: 2021/12/13 18:57:43 by aboudoun         ###   ########.fr       */
+/*   Updated: 2021/12/14 20:16:08 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"ft_printf.h"
 
-void	ft_putchar(char c, *len)
+void	ft_putchar(char c, int *len)
 {
 	write(1, &c, 1);
-	*count ++;
+	*len++;
 }
 
 void	ft_putnbr(int nb , int *len)
@@ -23,6 +23,7 @@ void	ft_putnbr(int nb , int *len)
     if ( nb == -2147483648)
 	{
 	   write(1, "-2147483648", 11);
+	   *len = 11;
 		return ;
     }
 	else if (nb < 0)
@@ -38,5 +39,18 @@ void	ft_putnbr(int nb , int *len)
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
+	}
+}
+
+void	ft_putstr(char *str, int *len)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+		*len++;
 	}
 }
